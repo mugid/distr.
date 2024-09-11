d690d7ef497c8a3625e806d5c1483df5b68d0649
MugiD <bekslambek22@gmail.com>
2024-09-12 04:21:22 +0500

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -42,6 +43,7 @@ export default function RootLayout({
               Bek Slambek
             </a>
           </footer>
+          <Analytics />
       </body>
     </html>
   );
