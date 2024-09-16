0938008aaf10763b3df1dd6080bbb15bf4226624
MugiD <bekslambek22@gmail.com>
2024-09-16 19:30:22 +0500

    fix: dropdown does not change the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Problify",
-  description: "Save time spent on calculations required to build the table, problify will count everything for you.",
+  description:
+    "Save time spent on calculations required to build the table, problify will count everything for you.",
 };
 
 export default function RootLayout({
@@ -28,30 +29,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-3xl mx-auto my-4 px-4`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <body>
+        <main
+          className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-3xl mx-auto my-4 px-4`}
         >
-          <Navbar />
-          <div>{children}</div>
-          <footer className="mt-8 pb-4 text-center text-sm text-gray-300">
-            Problem solved by{" "}
-            <a
-              href="https://instagram.com/sbek22"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-bold hover:underline text-blue-400"
-            >
-              Bek Slambek
-            </a>
-          </footer>
-        </ThemeProvider>
-        <Analytics />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            <div>{children}</div>
+            <footer className="mt-8 pb-4 text-center text-sm text-gray-300">
+              Problem solved by{" "}
+              <a
+                href="https://instagram.com/sbek22"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold hover:underline text-blue-400"
+              >
+                Bek Slambek
+              </a>
+            </footer>
+          </ThemeProvider>
+          <Analytics />
+        </main>
       </body>
     </html>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "./theme-button";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center">
-      <div className="flex gap-x-4">
+      <div className="flex items-center gap-x-4">
         <Link className="font-bold" href="/">problify</Link>
         <Link href="/create">create</Link>
       </div>
