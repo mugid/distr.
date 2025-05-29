81f9ec613c550429902d9f6aa6e3fc0530937d69
MugiD <bekslambek22@gmail.com>
2025-05-29 17:37:54 +0500

    rearrange: load fonts from google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+const geistSans = Geist({
   variable: "--font-geist-sans",
-  weight: "100 900",
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  weight: "100 900",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Problify",
+  title: "distr.",
   description:
     "Save time spent on calculations required to build the table, problify will count everything for you.",
 };
