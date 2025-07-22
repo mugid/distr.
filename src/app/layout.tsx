59958609b0113ee551182fa3a942884e9f5643ae
mugid <bekslambek22@gmail.com>
2025-07-22 22:22:45 +0500

    fix: set up correct colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,13 @@ import Navbar from "@/components/layout/navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -29,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
