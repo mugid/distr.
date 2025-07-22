fc8c908482f2c4c609f7c37399a5e8f686681d14
mugid <bekslambek22@gmail.com>
2025-07-22 19:13:57 +0500

    feat: add logo

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+export default function  Navbar() {
+  return (
+    <div className="flex justify-center items-center py-8 px-10">
+        <Link className="font-bold" href="/">
+        <Image
+          src="/logo-light.svg"
+          alt="distr. logo light"
+          width={80}
+          height={80}
+          className="inline-block dark:hidden"
+        />
+        <Image
+          src="/logo-dark.svg"
+          alt="distr. logo dark"
+          width={80}
+          height={80}
+          className="dark:inline-block hidden"
+        />
+        </Link>
+      </div>
+  );
+};
