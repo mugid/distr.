141d08ca0d12aab6ff27673d82f160bfab0ba470
mugid <bekslambek22@gmail.com>
2025-07-22 23:54:53 +0500

    fix: enable subsets for font preload

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -5,6 +5,8 @@ import { Jersey_15 } from "next/font/google";
 
 const jersey = Jersey_15({
   weight: "400",
+  subsets: ["latin"],
+  display: "swap",
 })
 
 export default function HeroSection() {
@@ -13,9 +15,9 @@ export default function HeroSection() {
       className="flex md:flex-row flex-col justify-start items-start md:items-center gap-12 py-40"
     >
       <h1 className="text-foreground text-4xl font-extrabold tracking-tighter lg:text-7xl">
-        Build <span className={`${jersey.className} text-8xl`}>tables and graphs</span>
+        Build <span className={`${jersey.className} text-5xl md:text-8xl`}>tables and graphs</span>
         <br className="md:block hidden" /> with{" "}
-        <span className={`${jersey.className} text-8xl text-primary`}>high</span> efficiency
+        <span className={`${jersey.className} text-5xl md:text-8xl text-primary`}>high</span> efficiency
       </h1>
       <button className="rounded-full bg-foreground px-6 py-2 font-medium text-background hover:bg-foreground/90 hover:px-8 transition-all">
         <Link href="/create">
