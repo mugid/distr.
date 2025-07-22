1af66b4f296c38f23a9b3c33016ed82d908ef804
mugid <bekslambek22@gmail.com>
2025-07-22 22:31:43 +0500

    fix: change logo size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 
 import FeaturesSection from "@/components/layout/features";
 import HeroSection from "@/components/layout/hero";
+import { motion } from "motion/react";
 
 export default function LandingPage() {
   return (
-    <div className="max-w-7xl px-10 mx-auto">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-7xl px-10 mx-auto"
+    >
       <HeroSection />
       <FeaturesSection />
-    </div>
+    </motion.div>
   );
 }
