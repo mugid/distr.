c49574b94283dccaa40e4090832481d5293b5c28
mugid <bekslambek22@gmail.com>
2025-07-22 22:31:21 +0500

    feat: add hover to button

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -1,18 +1,22 @@
 "use client";
 import Link from "next/link";
-import { motion } from "motion/react";
-import {ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon } from "lucide-react";
 
 export default function HeroSection() {
   return (
-    <motion.section className="flex md:flex-row flex-col justify-start items-start md:items-center gap-12 py-40">
+    <section
+      className="flex md:flex-row flex-col justify-start items-start md:items-center gap-12 py-40"
+    >
       <h1 className="text-foreground text-4xl font-extrabold tracking-tighter lg:text-7xl">
         Build <span className="font-medium">tables and graphs</span>
-        <br className="md:block hidden" /> with <span className="font-medium text-primary">high</span> efficiency
+        <br className="md:block hidden" /> with{" "}
+        <span className="font-medium text-primary">high</span> efficiency
       </h1>
-        <button className="rounded-full bg-foreground px-6 py-2 font-medium text-background">
-          <Link href="/create"><ArrowRightIcon width={36} height={36} /></Link>
-        </button>
-    </motion.section>
+      <button className="rounded-full bg-foreground px-6 py-2 font-medium text-background hover:bg-foreground/90 hover:px-8 transition-all">
+        <Link href="/create">
+          <ArrowRightIcon width={36} height={36}/>
+        </Link>
+      </button>
+    </section>
   );
 }
