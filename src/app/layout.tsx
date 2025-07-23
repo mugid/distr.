d694dc9cf81b16f7651e8feff4d283a03e76f8d4
mugid <bekslambek22@gmail.com>
2025-07-23 22:49:06 +0500

    feat: enable analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import Footer from "@/components/layout/footer";
